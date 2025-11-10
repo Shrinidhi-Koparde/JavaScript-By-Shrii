61324a8151287a3ef306c77a281b2e0524d54552
Shrinidhi Koparde <shrinidhikoparde@gmail.com>
2025-11-10 20:18:12 +0530

    Updated JS files

---
+++ Basics/01_variables.js
@@ -0,0 +1,9 @@
+const acc_ID = "123434"
+let acc_email = "shrinidhi@gmail.com"
+var acc_password  = "9878675"
+acc_City = "Dharwad"
+
+//use let inside nof var to have block scope
+
+console.log(acc_ID);
+console.table([acc_ID , acc_email , acc_password , acc_City])
---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log(2+29);
+let name = "shri"
+let age = 15
+let isprime = false
